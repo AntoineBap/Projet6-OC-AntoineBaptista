eeaf1fe4e144f9d688000da0d28ecfce7860bf6c
AntoineBap <129936013+AntoineBap@users.noreply.github.com>
2025-05-07 11:22:14 +0200

    Update book.js

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -4,11 +4,18 @@ const auth = require('../middleware/auth');
 const { upload, processImage } = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/book');
 
+// Routes de consultation
 router.get("/", bookCtrl.getAllBooks);
 router.get("/bestrating", bookCtrl.bestRating);
 router.get("/:id", bookCtrl.getOneBook);
+
+// Création avec image (upload + traitement + Cloudinary)
 router.post("/", auth, upload, processImage, bookCtrl.createBook);
+
+// Modification sans image pour l’instant (on peut l’ajouter après)
 router.put("/:id", auth, bookCtrl.modifyBook);
+
+// Suppression et notation
 router.delete("/:id", auth, bookCtrl.deleteBook);
 router.post("/:id/rating", auth, bookCtrl.rateBook);
 
